69eabac74fb3dff2e41d35d0381f7831fec5b0ce
Klipspringer <klipspringr@users.noreply.github.com>
2024-05-23 09:28:11 +0100

    caching for local dev

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ Available at <https://klipspringr.github.io/mefi-activity/>
 
 To remind myself how this works in future:
 
-- GitHub Action `infodump` runs `download.py` daily
-- `download.py` checks "last updated" timestamp on [infodump page](https://stuff.metafilter.com/infodump/). We expect updates roughly every month or two
-- if new infodump, download post comment and user data and parse into `public/data.json`
-- push new json to repo (requires Personal Access Token, stored in `INFODUMP_ACCESS_TOKEN` repo secret)
-- pushes trigger GitHub Action `deploy` to upload `public/` to GitHub Pages
+- `infodump` GitHub workflow calls `download.py` daily
+- `download.py` checks "last updated" timestamp on [infodump page](https://stuff.metafilter.com/infodump/). We expect updates every month or two
+- if new infodump, download post data, comment data, and usernames
+- parse into `public/data.json` and push to repo
+- pushes trigger the `deploy` workflow to upload `public/` to GitHub pages
+- in order for automated pushes from `infodump` to trigger `deploy`, a Personal Access Token must be stored in a `INFODUMP_ACCESS_TOKEN` repo secret
+
+For local development, manually download `postdata_*.txt.zip`, `commentdata_*.txt.zip` and `usernames.txt.zip`. Unzip and store in `cached_infodump/`. `download.py` reads these files, to avoid re-downloading from mefi.us every time the json is regenerated.
+
+In live usage, the `infodump` workflow calls `download.py` with the `--no-cache` arg. We only download infodump data if the page timestamp indicates the data is new.
--- download.py
+++ download.py
@@ -4,13 +4,18 @@ from datetime import datetime
 from io import BytesIO
 from urllib.request import urlopen, Request
 from zipfile import ZipFile
+import argparse
+import io
 import json
+import os
 import re
 import sys
 
 INFODUMP_HOMEPAGE = "https://stuff.metafilter.com/infodump/"
 INFODUMP_BASE = "https://mefi.us/infodump/"
 HEADERS = { "User-Agent": "github.com/klipspringr/mefi-activity" }
+
+CACHE_DIR = "cached_infodump"
 
 SITES = ["mefi", "askme", "meta", "fanfare", "music"]
 
@@ -23,91 +28,102 @@ def get_infodump_timestamp():
         raw_date = re.search("Last updated: <b>(.+)</b>", contents).group(1)
         parsed_date = datetime.strptime(raw_date, "%a %b %d %H:%M:%S %Y")
         return parsed_date.strftime("%d %B %Y %H:%M"), parsed_date.strftime("%Y-%b")
+    
+def read_file(reader, skip = 2):
+    for _ in range(skip):
+        reader.readline() # ignore initial lines
+    while line := reader.readline():
+        parts = line.replace("\n", "").split("\t")
+        if len(parts) < 2 or not parts[0].isnumeric():
+            continue # skip lines that can't be split by tab, or where first field is not numeric
+        yield parts
 
-def get_infodump_file(filename, skip = 2):
-    url = INFODUMP_BASE + filename + ".txt.zip"
-    print(f"get {url}")
+def get_infodump_file(filename):
+    if args.no_cache:
+        url = INFODUMP_BASE + filename + ".txt.zip"
+        print(f"get {url}")
+        with urlopen(Request(url, headers=HEADERS)) as f:
+            with ZipFile(BytesIO(f.read())) as zip:
+                with zip.open(filename + ".txt") as txt:
+                    yield from read_file(io.TextIOWrapper(txt, "utf-8"))
+    else:
+        path = os.path.join(CACHE_DIR, filename + ".txt")
+        print(f"read {path}")
+        with open(path, "r") as f:
+            yield from read_file(f)
 
-    with urlopen(Request(url, headers=HEADERS)) as f:
-        with ZipFile(BytesIO(f.read())) as zip:
-            with zip.open(filename + ".txt") as txt:
-                for _ in range(skip):
-                    txt.readline() # ignore initial lines
-                while line := txt.readline():
-                    parts = line.decode('utf-8').replace("\n", "").split("\t")
-                    if len(parts) < 2 or not parts[0].isnumeric():
-                        continue # skip lines that can't be split by tab, or where first field is not numeric
-                    yield parts
+def main():             
+    try:
+        with open(args.json_path, "r") as f:
+            json_timestamp = json.load(f)["infodump_timestamp"]
+    except:
+        json_timestamp = None
 
-if len(sys.argv) != 2 or sys.argv[1] == "":
-    raise Exception("Expected argument: path to output json")
+    infodump_timestamp, month_to_skip = get_infodump_timestamp()
+    if infodump_timestamp == json_timestamp:
+        print("infodump unchanged")
+        sys.exit(0)
 
-json_path = sys.argv[1]
+    user_joined = {}
+    for user_id, date, _ in get_infodump_file("usernames"):
+        user_joined[user_id] = date[7:11]
 
-try:
-    with open(json_path, "r") as f:
-        json_timestamp = json.load(f)["infodump_timestamp"]
-except:
-    json_timestamp = None
+    posts = {site: defaultdict(int) for site in SITES}
+    comments = {site: defaultdict(int) for site in SITES}
+    unique_users = {site: defaultdict(set) for site in SITES}
 
-infodump_timestamp, month_to_skip = get_infodump_timestamp()
-if infodump_timestamp == json_timestamp:
-    print("infodump unchanged")
-    sys.exit(0)
+    posts["all"] = defaultdict(int)
+    comments["all"] = defaultdict(int)
+    unique_users["all"] = defaultdict(set)
 
-user_joined = {}
-for user_id, date, _ in get_infodump_file("usernames"):
-    user_joined[user_id] = date[7:11]
+    for site in SITES:
+        for _, user_id, date, category, *_ in get_infodump_file(f"postdata_{site}"):
+            if site == "meta" and category == "10":
+                continue # skip early askmes stored in meta db table
+            month = f'{date[7:11]}-{date[:3]}'
+            if month == month_to_skip:
+                break
+            posts[site][month] += 1
+            posts["all"][month] += 1
+            unique_users[site][month].add(user_id)
+            unique_users["all"][month].add(user_id)
 
-posts = {site: defaultdict(int) for site in SITES}
-comments = {site: defaultdict(int) for site in SITES}
-unique_users = {site: defaultdict(set) for site in SITES}
+        for _, _, user_id, date, *_ in get_infodump_file(f"commentdata_{site}"):
+            month = f'{date[7:11]}-{date[:3]}'
+            if month == month_to_skip:
+                break
+            comments[site][month] += 1
+            comments["all"][month] += 1
+            unique_users[site][month].add(user_id)
+            unique_users["all"][month].add(user_id)
 
-posts["all"] = defaultdict(int)
-comments["all"] = defaultdict(int)
-unique_users["all"] = defaultdict(set)
+    active = {}
+    for site, months_users in unique_users.items():
+        active[site] = {
+            "labels": [k for k in months_users.keys()],
+            "by_year": {join_year: [0 for _ in months_users.keys()] for join_year in JOIN_YEARS},
+            "totals": [len(u) for u in months_users.values()]
+        }
+        for index, (month, users) in enumerate(months_users.items()):
+            for user_id in users:
+                # a few user_ids are not in user_joined: see https://mefiwiki.com/wiki/Infodump#Userid_munging
+                if user_id in user_joined:
+                    join_year = user_joined[user_id]
+                    active[site]["by_year"][join_year][index] += 1
 
-for site in SITES:
-    for _, user_id, date, category, *_ in get_infodump_file(f"postdata_{site}"):
-        if site == "meta" and category == "10":
-            continue # skip early askmes stored in meta db table
-        month = f'{date[7:11]}-{date[:3]}'
-        if month == month_to_skip:
-            break
-        posts[site][month] += 1
-        posts["all"][month] += 1
-        unique_users[site][month].add(user_id)
-        unique_users["all"][month].add(user_id)
+    with open(args.json_path, "w") as f:
+        json.dump({
+            "infodump_timestamp": infodump_timestamp,
+            "posts": posts,
+            "comments": comments,
+            "active": active
+        }, f)
 
-    for _, _, user_id, date, *_ in get_infodump_file(f"commentdata_{site}"):
-        month = f'{date[7:11]}-{date[:3]}'
-        if month == month_to_skip:
-            break
-        comments[site][month] += 1
-        comments["all"][month] += 1
-        unique_users[site][month].add(user_id)
-        unique_users["all"][month].add(user_id)
+    print(f"wrote to {args.json_path}")
 
-active = {}
-for site, months_users in unique_users.items():
-    active[site] = {
-        "labels": [k for k in months_users.keys()],
-        "by_year": {join_year: [0 for _ in months_users.keys()] for join_year in JOIN_YEARS},
-        "totals": [len(u) for u in months_users.values()]
-    }
-    for index, (month, users) in enumerate(months_users.items()):
-        for user_id in users:
-            # a few user_ids are not in user_joined: see https://mefiwiki.com/wiki/Infodump#Userid_munging
-            if user_id in user_joined:
-                join_year = user_joined[user_id]
-                active[site]["by_year"][join_year][index] += 1
-
-with open(json_path, "w") as f:
-    json.dump({
-        "infodump_timestamp": infodump_timestamp,
-        "posts": posts,
-        "comments": comments,
-        "active": active
-    }, f)
-
-print(f"wrote to {json_path}")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("json_path")
+    parser.add_argument("--no-cache", action="store_true")
+    args = parser.parse_args()
+    main()
