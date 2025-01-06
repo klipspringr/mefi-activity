8de151bdcc4c9079c31242790148b8c7219fe277
Klipspringer <klipspringr@users.noreply.github.com>
2025-01-06 22:23:26 +0000

    refactor USER_AGENT to repo secret

--- README.md
+++ README.md
@@ -9,10 +9,11 @@ To remind myself how it works in future:
 ## GitHub scheduled action
 
 - `infodump` workflow calls `parser/download.py` regularly
-- script checks "last updated" timestamp on [Infodump homepage](https://stuff.metafilter.com/infodump/). We expect updates every few months
+- script checks "last updated" timestamp on [Infodump homepage](https://stuff.metafilter.com/infodump/)
 - if a new Infodump is available, download postdata, commentdata, and usernames to `infodump/`, and parse into `src/data/data.json`
 - push json to repo
-- for pushes to trigger a deployment, a Personal Access Token must be stored in a `INFODUMP_ACCESS_TOKEN` repo secret
+- a Personal Access Token must be stored in the `INFODUMP_ACCESS_TOKEN` repo secret, so pushes trigger a deployment
+- `USER_AGENT` repo secret is also required
 
 ## Static frontend
 
@@ -21,6 +22,10 @@ To remind myself how it works in future:
 ## Local development
 
 - create a venv and `pip install -r parser/requirements.txt`
-- run `python parser/download.py -d infodump src/data/data.json`
+- run `python parser/download.py -d YOUR_USER_AGENT infodump src/data/data.json`
 - with the `-d|--dev` flag, we (a) download Infodump data on first run (but not subsequently, unless a new Infodump is published), and (b) always regenerate the json
-- `notebook.ipynb` makes testing polars expressions easier. It is not used in "production". Install Jupyter kernel requirements from `parser/requirements_notebook.txt`
+- `YOUR_USER_AGENT` is sent as the User-Agent header when downloading large Infodump files, and should be set to a string that identifies you
+
+## Notebook
+
+- `parser/notebook.ipynb` makes testing Polars expressions easier. It is not used to generate the live site. Install Jupyter kernel requirements from `parser/requirements_notebook.txt`. Some packages are included to support the Data Wrangler VS Code extension
--- parser/download.py
+++ parser/download.py
@@ -1,19 +1,19 @@
 import argparse
-from datetime import datetime
 import json
 import os
 import re
+from datetime import datetime
 from io import BytesIO
 from pathlib import Path
 from urllib.request import Request, urlopen
 from zipfile import ZipFile
 
-from config import HEADERS, INFODUMP_BASE_URL, INFODUMP_HOMEPAGE, SITES, KEY_TIMESTAMP
+from config import INFODUMP_BASE_URL, INFODUMP_HOMEPAGE, KEY_TIMESTAMP, SITES
 from parse import parse
 
 
 def get_publication_timestamp():
-    with urlopen(Request(INFODUMP_HOMEPAGE, headers=HEADERS)) as f:
+    with urlopen(INFODUMP_HOMEPAGE) as f:
         contents = f.read().decode("utf-8")
         raw_date = re.search("Last updated: <b>(.+)</b>", contents).group(1).strip()
         return datetime.strptime(raw_date, "%a %b %d %H:%M:%S %Y").strftime(
@@ -21,15 +21,15 @@ def get_publication_timestamp():
         )
 
 
-def download_file(filename, infodump_dir):
+def download_file(user_agent, filename, infodump_dir):
     url = INFODUMP_BASE_URL + filename + ".txt.zip"
     print(f"Download and extract {url}")
-    with urlopen(Request(url, headers=HEADERS)) as f:
+    with urlopen(Request(url, headers={"User-Agent": user_agent})) as f:
         with ZipFile(BytesIO(f.read())) as zip:
             zip.extract(filename + ".txt", infodump_dir)
 
 
-def download_infodump(dev, infodump_dir, output_path):
+def download_infodump(dev, user_agent, infodump_dir, output_path):
     publication_timestamp = get_publication_timestamp()
 
     download_required = True
@@ -47,22 +47,21 @@ def download_infodump(dev, infodump_dir, output_path):
                 download_required = False
 
     # download infodump if it is fresh, or if we are in dev mode and have not downloaded it already
-    if download_required or (
-        dev and not os.path.exists(os.path.join(infodump_dir, "usernames.txt"))
-    ):
+    infodump_path = Path(infodump_dir)
+    if download_required or (dev and not (infodump_path / "usernames.txt").exists()):
         print("Download Infodump")
-        if not os.path.isdir(infodump_dir):
-            print(f'Create dir "{infodump_dir}"')
-            os.mkdir(infodump_dir)
-        else:
-            print(f'Delete "{infodump_dir}/*.txt"')
-            for txt in Path(infodump_dir).glob("*.txt"):
-                txt.unlink()
 
-        download_file("usernames", infodump_dir)
+        if infodump_path.exists():
+            print(f'Delete "{infodump_dir}/*.txt"')
+            for txt in infodump_path.glob("*.txt"):
+                txt.unlink()
+        else:
+            infodump_path.mkdir()
+
+        download_file(user_agent, "usernames", infodump_dir)
         for site in SITES:
-            download_file(f"postdata_{site}", infodump_dir)
-            download_file(f"commentdata_{site}", infodump_dir)
+            download_file(user_agent, f"postdata_{site}", infodump_dir)
+            download_file(user_agent, f"commentdata_{site}", infodump_dir)
 
     if download_required or dev:
         print(f'Parsing data from "{infodump_dir}" to "{output_path}"')
@@ -72,7 +71,8 @@ def download_infodump(dev, infodump_dir, output_path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dev", action="store_true")
+    parser.add_argument("user_agent")
     parser.add_argument("infodump_dir")
     parser.add_argument("output_path")
     args = parser.parse_args()
-    download_infodump(args.dev, args.infodump_dir, args.output_path)
+    download_infodump(args.dev, args.user_agent, args.infodump_dir, args.output_path)
