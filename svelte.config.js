24e11bcf41f2d10c03ad7e1bd5c7054211f0108e
Klipspringer <klipspringr@users.noreply.github.com>
2026-02-14 13:11:50 +0000

    Revert "render a 404 page for github pages"
    This reverts commit 7b032eae60bc5a575f1f510079d42813e4eec420.

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
         adapter: adapter({
             pages: "build",
             assets: "build",
-            fallback: "404.html",
+            fallback: undefined,
             precompress: false,
             strict: true,
         }),
